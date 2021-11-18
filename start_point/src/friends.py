58a2a60ec6c10531fa8322f848254797fb62a10e
peterwilliams <peterkennethwilliams@gmail.com>
2021-11-18 14:52:17 +0000

    likes_to_eat

--- start_point/src/friends.py
+++ start_point/src/friends.py
@@ -3,3 +3,11 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person,food):
+    for snacks in (person["favourites"]["snacks"]):
+        if snacks == food:
+    
+             return True
+      
+    
